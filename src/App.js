65fe9eb6a18b43a1c4d7997823b3a3500fd1c9ad
mymehrnaz <165458618+mymehrnaz@users.noreply.github.com>
2025-09-06 14:49:09 +0330

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import TodoList from './TodoList';
+import Footer from './Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className='App-1'>
+      <h1>ToDo List</h1>
 <TodoList/>
+</div>
+<Footer/>
     </div>
+  
+
   );
+
 }
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css"
+
+function Footer(){
+    return(
+<h4>This site was coded by Mehrnaz Sadeghian</h4>
+    );
+}
+export default Footer;
